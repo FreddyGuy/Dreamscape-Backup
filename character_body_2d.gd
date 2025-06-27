17ccfcb4825f0ac66122bf052682d34f339e6cbb
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-06-27 12:34:59 +1200

    Term Commit

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -2,14 +2,17 @@ extends CharacterBody2D
 
 
 var WALK_SPEED = 200.0
-var SHORT_JUMP_VELOCITY = -200.0
+var SHORT_JUMP_HEIGHT = -200.0
 var SPRINT_SPEED = 450.0
 var HIGH_JUMP_VELOCITY = -450.0
-var FLOAT_SPEED = 800
+var FLOAT_SPEED = 800.0
+var STRUGGLE_JUMP = -150.0
+var MAX_FALL_SPEED = -300.0
 var IS_SPRINTING = false
 var CAN_SPRINT = false
 var IS_FLOATING = false
 var CAN_FLOAT = false
+var CAN_STRUGGLE_JUMP = false
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -18,7 +21,9 @@ func _physics_process(delta):
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = SHORT_JUMP_VELOCITY
+		velocity.y = SHORT_JUMP_HEIGHT
+	if Input.is_action_pressed("ui_accept") and not is_on_floor():
+		velocity.y = STRUGGLE_JUMP
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -32,6 +37,13 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, WALK_SPEED)
 	if Input.is_action_pressed("Sprint"):
 		WALK_SPEED = SPRINT_SPEED
+	if not is_on_floor():
+		velocity += CAN_FLOAT
+	if Input.is_action_pressed("Float") and CAN_FLOAT:
+		FLOAT_SPEED = true
+		$Float_Timer.start()
 		
-
 	move_and_slide()
+	
+	# Respawn System
+	
